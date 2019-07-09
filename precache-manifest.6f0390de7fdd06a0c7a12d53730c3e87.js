527a1d30e9864046278ad38935953445bd4a1586
Jake Stothard <jake.stothard@NWMML0Q6HV2F-010138142011.clients.global.arup.com>
2019-07-09 16:34:14 +0100

    Updates

--- precache-manifest.6f0390de7fdd06a0c7a12d53730c3e87.js
+++ precache-manifest.6f0390de7fdd06a0c7a12d53730c3e87.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7cb765938107c9cc2a241f8594a2fb9a",
+    "revision": "434086a49f629d33229b43b83b889bd0",
     "url": "/360-viewer/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/360-viewer/static/js/2.870bce39.chunk.js"
   },
   {
-    "revision": "0f932ee2d5f814050367",
-    "url": "/360-viewer/static/js/main.51a9dd02.chunk.js"
+    "revision": "08d3cc1bf4c85b348bc9",
+    "url": "/360-viewer/static/js/main.cda481c2.chunk.js"
   },
   {
     "revision": "4f07809d15e27d30ed55",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/360-viewer/precache-manifest.ec9004efac70e0a186100731695b6f2f.js"
+  "/360-viewer/precache-manifest.6f0390de7fdd06a0c7a12d53730c3e87.js"
 );
 
 self.addEventListener('message', (event) => {
